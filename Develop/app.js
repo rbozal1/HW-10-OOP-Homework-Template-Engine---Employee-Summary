126c856fc91c49f4c32e79f23048e154fc822a30
Roger Steve <roxhersbozali@gmail.com>
2020-10-28 00:22:56 -0400

    third commit

--- Develop/app.js
+++ Develop/app.js
@@ -3,18 +3,172 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+
 //packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
-
+const teamHTML = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function promptMember() {
+    console.log("It is time to build you team!")
+    return inquirer.prompt([
+    {
+        type:"input",
+        name:"managerName",
+        message: "What is your manager's name .? "
+    },
+    
+    {
+        type:"input",
+        name:"managerId",
+        message: "What is your manager's Id? "
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your manager's email? "
+    },
+
+    {
+        type: "input",
+        name: "managerOfficeNo",
+        message: "What is your manager's Office Number? "
+    },
+])
+.then((data) => {
+  const manager = new Manager(
+    data.managerName,
+    data.managerId,
+    data.managerEmail,
+    data.managerOfficeNo
+  );
+   teamHTML.push(manager);
+   console.log(`${data.managerName} has been added!`);
+   teamMember = fs.readFileSync("templates/manager.html");
+     
+
+      addAnother();
+    });
+} 
+function addEngineer() {
+    console.log("Lets add your Engineer");
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the engineer's name?",
+          name: "engineerName",
+        },
+        {
+          type: "input",
+          message: "What is the engineer's Id?",
+          name: "engineerId",
+        },
+        {
+          type: "input",
+          message: "What is the engineer's email?",
+          name: "engineerEmail",
+         
+        },
+        {
+          type: "input",
+          message: "What is the engineer's GitHub user name?",
+          name: "engineerGitHub",
+        },
+      ])
+      .then((data) => {
+        const engineer = new Engineer(
+          data.engineerName,
+          data.engineerId,
+          data.engineerEmail,
+          data.engineerGitHub
+        );
+        teamHTML.push(engineer);
+        console.log(`${data.engineerName} has been added!`);
+        teamMember = fs.readFileSync("templates/engineer.html");
+        
+        addAnother();
+      });
+  }
+  
+  function addIntern() {
+    console.log("Lets add your Intern");
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the intern's name?",
+          name: "internName"
+        },
+        {
+          type: "input",
+          message: "What is the intern's Id?",
+          name: "internId"
+        },
+        {
+          type: "input",
+          message: "What is the intern's email?",
+          name: "internEmail"
+        },
+        {
+          type: "input",
+          message: "What school does the intern attend?",
+          name: "internSchool"
+        },
+      ])
+      .then((data) => {
+        const intern = new Intern(
+          data.internName,
+          data.internId,
+          data.internEmail,
+          data.internSchool
+        );
+        teamHTML.push(intern);
+        console.log(`${data.internName} has been added!`);
+        teamMember = fs.readFileSync("templates/intern.html");
+        
+        // teamHTML.push(intern);
+        addAnother();
+      });
+  }
+  
+  function addAnother() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Chose the role of your next team member",
+          name: "choice",
+          choices: ["Add Engineer", "Add Intern", "Done Adding"],
+        },
+      ]).then(function (data) {
+        if (data.choice === "Add Engineer") {
+          addEngineer();
+        } else if (data.choice === "Add Intern") {
+          addIntern();
+        } else (outputTeam());
+        
+      });
+    };
+
+    function outputTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamHTML), "utf-8");
+    }
+
+  promptMember();
 
 
 
